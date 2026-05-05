4926ea56afffd2ed622d99192bf452fd7d8a5e28
YousefBadawy <125797361+YousefBadawy@users.noreply.github.com>
2026-05-05 06:13:23 +0300

    Add placement.py with pin and cell placement functions

---
+++ parsing/placement.py
@@ -0,0 +1,11 @@
+import random
+import sys
+
+def place_pins(components, occupant_grid, site_grid, nx, ny):
+    pass
+
+def place_cells(components, occupant_grid, empty_sites):
+    pass
+
+def check_placement(components, occupant_grid, site_grid, nx, ny):
+    pass
